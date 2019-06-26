76f9335fbd4b8c41531c64c4081ed6b1abe0f710
Reggie Davis <reginald.davis88@gmail.com>
2019-06-25 22:20:32 -0500

    Added json file and ability to open file

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
+// Users represents a collection of users
+type Users struct {
+	Users []User `json:"users"`
+}
+
+// User is a person who uses our services
+type User struct {
+	Name   string `json:"name"`
+	Type   string `json:"type"`
+	Age    int64  `json:"age"`
+	Social Social `json:"social"`
+}
+
+// Social is the soclia media info of our users
+type Social struct {
+	Facebook string `json:"facebook"`
+	Twitter  string `json:"twitter"`
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	jsonFile, err := os.Open("./test.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var users Users
+
+	json.Unmarshal(byteValue, &users)
+
+	for _, user := range users.Users {
+		fmt.Printf("This is %v\n", user.Name)
+	}
 }
